7514a024c250ae1d50f2f6b6456d4a89df2e2cd9
Haeryz00 <muh4mm4dh4r1z@gmail.com>
2023-04-15 14:07:22 +0700

    First commit

---
+++ src/disc/orang/pelanggan.java
@@ -0,0 +1,15 @@
+package disc.orang;
+
+import disc.memership;
+import disc.person;
+
+public class pelanggan extends person implements memership{
+    public String memberType;
+
+    @Override
+    public double getMebershipdisc() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMebershipdisc'");
+    }
+    
+}
